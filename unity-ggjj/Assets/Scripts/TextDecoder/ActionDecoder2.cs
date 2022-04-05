e1c679faf2bb40cacba31a77ff2d1fd2e24dd2ba
Phoebe Mitchell <67964179+PhoebeMitchell@users.noreply.github.com>
2022-04-05 05:06:00 +0100

    Removed unused namespaces

--- unity-ggjj/Assets/Scripts/TextDecoder/ActionDecoder2.cs
+++ unity-ggjj/Assets/Scripts/TextDecoder/ActionDecoder2.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
-using UnityEngine;
-using UnityEngine.Events;
 
 namespace TextDecoding
 {
